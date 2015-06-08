3d3f7e1dbb26522487cfb0a10e2e48fc50c0561e
AmitaiB <blickstein@gmail.com>
2015-06-08 05:12:33 -0400

    adds new Person class, and some properties

---
+++ ObjectOrientedPeople/Person.h
@@ -0,0 +1,20 @@
+//
+//  Person.h
+//  ObjectOrientedPeople
+//
+//  Created by Amitai Blickstein on 6/8/15.
+//  Copyright (c) 2015 al-tyus.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@property (strong, nonatomic) NSNumber *height;
+@property (strong, nonatomic) NSNumber *age;
+@property (strong, nonatomic) NSString *name;
+@property BOOL isFemale;
+@property NSMutableArray *friends;
+
+
+@end
---
+++ ObjectOrientedPeople/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  ObjectOrientedPeople
+//
+//  Created by Amitai Blickstein on 6/8/15.
+//  Copyright (c) 2015 al-tyus.com. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
